4449e4730bf274fc1c6a77b7c09868bb4f7ba393
Gonzalo Martone <gmartone@itba.edu.ar>
2023-04-03 19:41:15 -0300

    feat: add enpoints to controller

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
@@ -13,41 +13,66 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HelloWorldController {
 
-    private final UserService userService;
+  private final UserService userService;
 
-    @Autowired
-    public HelloWorldController(final UserService userService) {
-        this.userService = userService;
-    }
+  @Autowired
+  public HelloWorldController(final UserService userService) {
+    this.userService = userService;
+  }
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public ModelAndView helloWorld() {
-        final ModelAndView mav = new ModelAndView("helloworld/hello");
-        mav.addObject("user", userService.createUser("pepe@pepe.com", "secreta"));
+  @RequestMapping(value = "/hello", method = RequestMethod.GET)
+  public ModelAndView helloWorld() {
+    final ModelAndView mav = new ModelAndView("helloworld/hello");
+    mav.addObject("user", userService.createUser("pepe@pepe.com", "secreta"));
 
-        return mav;
-    }
+    return mav;
+  }
 
-    @RequestMapping("/{id}")
-    public ModelAndView profile(@PathVariable("id") final long userId) {
-        final ModelAndView mav = new ModelAndView("helloworld/profile");
-        mav.addObject("userId", userId);
+  @RequestMapping("/{id}")
+  public ModelAndView profile(@PathVariable("id") final long userId) {
+    final ModelAndView mav = new ModelAndView("helloworld/profile");
+    mav.addObject("userId", userId);
 
-        return mav;
-    }
+    return mav;
+  }
 
-    @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public ModelAndView register(@RequestParam(value = "email", required = true) final String email,
-                                 @RequestParam(value = "password", required = true) final String password) {
-        final User user = userService.createUser(email, password);
+  @RequestMapping(value = "/register", method = RequestMethod.POST)
+  public ModelAndView register(
+      @RequestParam(value = "email", required = true) final String email,
+      @RequestParam(value = "password", required = true) final String password) {
+    final User user = userService.createUser(email, password);
 
-        final ModelAndView mav = new ModelAndView("helloworld/hello");
-        mav.addObject("user", user);
-        return mav;
-    }
+    final ModelAndView mav = new ModelAndView("helloworld/hello");
+    mav.addObject("user", user);
+    return mav;
+  }
 
-    @RequestMapping(value = "/register", method = RequestMethod.GET)
-    public ModelAndView registerForm() {
-        return new ModelAndView("helloworld/register");
-    }
+  @RequestMapping(value = "/register", method = RequestMethod.GET)
+  public ModelAndView registerForm() {
+    return new ModelAndView("helloworld/register");
+  }
+
+  // TODO: revisar campos
+  @RequestMapping(value = "/register_medic", method = RequestMethod.POST)
+  public ModelAndView registerMedicForm(
+      @RequestParam(value = "name", required = true) final String name,
+      @RequestParam(value = "lastname", required = true) final String lastname,
+      @RequestParam(value = "address", required = true) final String address,
+      @RequestParam(value = "city", required = true) final String city,
+      @RequestParam(value = "specialization", required = true) final String specialization,
+      // TODO: buscar nombre para "obra social" :D
+      @RequestParam(value = "obra_social", required = true) final String obra_social,
+      @RequestParam(value = "email", required = true) final String email,
+      @RequestParam(value = "password", required = true) final String password) {
+    final User user = userService.createUser(email, password);
+
+    final ModelAndView mav = new ModelAndView("helloworld/hello");
+    mav.addObject("user", user);
+    return mav;
+  }
+
+  @RequestMapping(value = "/register_medic", method = RequestMethod.GET)
+  public ModelAndView registerMedicForm() {
+    return new ModelAndView("helloworld/register_medic");
+  }
 }
