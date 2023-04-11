e70f509f78f0c19ac8db6de04aa382da5c78a2cc
Gonzalo Martone <gmartone@itba.edu.ar>
2023-04-10 21:42:47 -0300

    refactor: rename & relocate classes v2

--- services/src/main/java/ar/edu/itba/paw/services/MailServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/MailServiceImpl.java
@@ -1,5 +1,6 @@
 package ar.edu.itba.paw.services;
 
+import ar.edu.itba.paw.interfaces.services.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -7,45 +8,52 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MailServiceImpl implements MailService {
-    
-    private static final String FROM = "noreply@turnosya.com";
 
-    private final JavaMailSender mailSender;
+  private static final String FROM = "noreply@turnosya.com";
 
+  private final JavaMailSender mailSender;
 
-    @Autowired
-    public MailServiceImpl(JavaMailSender mailSender){
-        this.mailSender = mailSender;
-    }
+  @Autowired
+  public MailServiceImpl(JavaMailSender mailSender) {
+    this.mailSender = mailSender;
+  }
 
-    private void sendMail(String to, String subject, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(FROM);
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
-        mailSender.send(message);
-    }
+  private void sendMail(String to, String subject, String text) {
+    SimpleMailMessage message = new SimpleMailMessage();
+    message.setFrom(FROM);
+    message.setTo(to);
+    message.setSubject(subject);
+    message.setText(text);
+    mailSender.send(message);
+  }
 
-    @Override
-    public void sendAppointmentRequestMail(String clientEmail, String doctorEmail, String clientName, String healthCare, String date, String description) {
+  @Override
+  public void sendAppointmentRequestMail(
+      String clientEmail,
+      String doctorEmail,
+      String clientName,
+      String healthCare,
+      String date,
+      String description) {
 
-        String to = doctorEmail;
-        String subject = "TurnosYa - Solicitud de turno";
+    String to = doctorEmail;
+    String subject = "TurnosYa - Solicitud de turno";
 
-        // Create mail body
-        StringBuilder body = new StringBuilder();
+    // Create mail body
+    StringBuilder body = new StringBuilder();
 
-        body.append("El usuario ").append(clientName).append(" ha solicitado un turno de ser posible en la siguiente fecha: ");
-        body.append(date).append("\n\n");
+    body.append("El usuario ")
+        .append(clientName)
+        .append(" ha solicitado un turno de ser posible en la siguiente fecha: ");
+    body.append(date).append("\n\n");
 
-        body.append("Descripcion:\n").append(description).append("\n\n");
+    body.append("Descripcion:\n").append(description).append("\n\n");
 
-        body.append("Datos del paciente:").append("\n\n");
-        body.append("Nombre: ").append(clientName).append('\n');
-        body.append("Obra social: ").append(healthCare).append('\n');
-        body.append("Email: ").append(clientEmail).append('\n');
-        
-        sendMail(to, subject, body.toString());
-    }
+    body.append("Datos del paciente:").append("\n\n");
+    body.append("Nombre: ").append(clientName).append('\n');
+    body.append("Obra social: ").append(healthCare).append('\n');
+    body.append("Email: ").append(clientEmail).append('\n');
+
+    sendMail(to, subject, body.toString());
+  }
 }
--- services/src/test/java/ar/edu/itba/paw/services/UserServiceImplTest.java
+++ services/src/test/java/ar/edu/itba/paw/services/UserServiceImplTest.java
@@ -1,7 +1,7 @@
 package ar.edu.itba.paw.services;
 
+import ar.edu.itba.paw.interfaces.persistence.UserDao;
 import ar.edu.itba.paw.models.User;
-import ar.edu.itba.paw.persistence.UserDao;
 import java.util.Optional;
 import org.junit.Assert;
 import org.junit.Test;
