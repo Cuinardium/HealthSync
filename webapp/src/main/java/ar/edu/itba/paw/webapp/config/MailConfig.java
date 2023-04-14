79f495a18f68cbfe2cbbff478bea4e28baafbdb9
Cuinardium <sballerini@itba.edu.ar>
2023-04-14 18:14:18 -0300

    feat(mail-template): setup mail-template config

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/MailConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/MailConfig.java
@@ -1,33 +1,75 @@
 package ar.edu.itba.paw.webapp.config;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Properties;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @Configuration
-@ComponentScan({"ar.edu.itba.paw.services"})
+@ComponentScan({"ar.edu.itba.paw.interfaces.services"})
 public class MailConfig {
 
-    @Bean
-    public JavaMailSender javaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
+  @Bean
+  public JavaMailSender javaMailSender() {
+    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+    mailSender.setHost("smtp.gmail.com");
+    mailSender.setPort(587);
 
-        mailSender.setUsername("paw.turnosya@gmail.com");
-        mailSender.setPassword("wgfcaezyfjpuxapo");
+    mailSender.setUsername("paw.turnosya@gmail.com");
+    mailSender.setPassword("wgfcaezyfjpuxapo");
 
+    Properties properties = mailSender.getJavaMailProperties();
+    properties.put("mail.transport.protocol", "smtp");
+    properties.put("mail.smtp.auth", "true");
+    properties.put("mail.smtp.starttls.enable", "true");
+    properties.put("mail.debug", "true");
 
-        Properties properties = mailSender.getJavaMailProperties();
-        properties.put("mail.transport.protocol", "smtp");
-        properties.put("mail.smtp.auth", "true");
-        properties.put("mail.smtp.starttls.enable", "true");
-        properties.put("mail.debug", "true");
+    return mailSender;
+  }
 
-        return mailSender;
-    }
+  // Resolver for locating mail templates
+  // Located un resources/mailTemplates
+  @Bean
+  public ITemplateResolver thymeleafTemplateResolver() {
+    ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+
+    templateResolver.setPrefix("mailTemplates/");
+    templateResolver.setSuffix(".html");
+    templateResolver.setTemplateMode("HTML");
+    templateResolver.setCharacterEncoding(StandardCharsets.UTF_8.displayName());
+    templateResolver.setOrder(0);
+
+    return templateResolver;
+  }
+
+  // Message source for translations
+  // Located in resources/i18n
+  // With file format mailMessages_xx_YY.properties
+  @Bean
+  public ResourceBundleMessageSource emailMessageSource() {
+    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+
+    messageSource.setCacheSeconds(5);
+    messageSource.setBasename("i18n/mailMessages");
+    messageSource.setDefaultEncoding(StandardCharsets.UTF_8.name());
+
+    return messageSource;
+  }
+
+  @Bean
+  public SpringTemplateEngine thymeleafTemplateEngine(ITemplateResolver templateResolver) {
+    SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+
+    templateEngine.setTemplateResolver(templateResolver);
+    templateEngine.setTemplateEngineMessageSource(emailMessageSource());
+
+    return templateEngine;
+  }
 }
