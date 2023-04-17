eb72ab857ac45965afb520a4c928e9d49e7c6358
Santiago Ballerini <sballerini@itba.edu.ar>
2023-04-17 14:36:22 -0300

    feat: add query params to doctor dashboard

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
@@ -153,10 +153,15 @@ public class HelloWorldController {
   }
 
   @RequestMapping(value = "/doctorDashboard", method = RequestMethod.GET)
-  public ModelAndView doctorDashboard() {
+  public ModelAndView doctorDashboard(
+    @RequestParam(value = "city", required = false) final String city,
+    @RequestParam(value = "specialty", required = false) final String specialty,
+    @RequestParam(value = "healthcare", required = false) final String healthcare
+  ) {
     final ModelAndView mav = new ModelAndView("helloworld/doctorDashboard");
 
-    List<Doctor> doctors = doctorService.getDoctors();
+    List<Doctor> doctors = doctorService.getFilteredDoctors(specialty, city, healthcare);
+
     mav.addObject("doctors", doctors);
 
     return mav;
