488b851fdb7c6cb10842f7236899f39654d8d1e9
Gonzalo Martone <gmartone@itba.edu.ar>
2023-04-18 14:04:23 -0300

    fix: package was missing

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorController.java
@@ -1,3 +1,5 @@
+package ar.edu.itba.paw.webapp.controller;
+
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
