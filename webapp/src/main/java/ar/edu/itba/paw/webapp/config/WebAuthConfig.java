e9d1c9959d2ed46b01b1934bce0188d3a64af323
Gonzalo Martone <gmartone@itba.edu.ar>
2023-04-24 19:35:48 -0300

    fix: changed wrong routes in webAuth configuration

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
@@ -1,5 +1,6 @@
 package ar.edu.itba.paw.webapp.config;
 
+import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,55 +14,66 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.concurrent.TimeUnit;
-
 @EnableWebSecurity
 @ComponentScan({"ar.edu.itba.paw.webapp.auth"})
 @Configuration
 public class WebAuthConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private UserDetailsService userDetailsService;
+  @Autowired private UserDetailsService userDetailsService;
 
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-    }
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+  }
 
-    @Override
-    protected void configure(final HttpSecurity http) throws Exception{
-        http.sessionManagement()
-                    .invalidSessionUrl("/login")
-                .and().authorizeRequests()
-                    .antMatchers("/login", "/register").anonymous()//TODO PONER DESP DE ESTA LINEA REGLAS DE CONTROL DE ACCESO DE ROLES CON .hasRole()
-                    .antMatchers("/**").authenticated()
-                .and().formLogin()
-                    .loginPage("/login")
-                    .usernameParameter("email")
-                    .passwordParameter("password")
-                    .defaultSuccessUrl("/", false)
-                .and().rememberMe()
-                    .rememberMeParameter("rememberme")
-                    .userDetailsService(userDetailsService)
-                    .key("NO HAGAS ESTO")
-                    .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(30))
-                .and().logout()
-                    .logoutUrl("/logout")
-                    .logoutSuccessUrl("/login")
-                .and().exceptionHandling()
-                    .accessDeniedPage("/errors")
-                .and().csrf().disable();
-    }
+  @Override
+  protected void configure(final HttpSecurity http) throws Exception {
+    http.sessionManagement()
+        .invalidSessionUrl("/login")
+        .and()
+        .authorizeRequests()
+        .antMatchers("/login", "/register")
+        .anonymous() // TODO PONER DESP DE ESTA LINEA REGLAS DE CONTROL DE ACCESO DE ROLES CON
+        // .hasRole()
+        .antMatchers("/**")
+        .authenticated()
+        .and()
+        .formLogin()
+        .loginPage("/login")
+        .usernameParameter("email")
+        .passwordParameter("password")
+        .defaultSuccessUrl("/", false)
+        .and()
+        .rememberMe()
+        .rememberMeParameter("rememberme")
+        .userDetailsService(userDetailsService)
+        .key("NO HAGAS ESTO")
+        .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(30))
+        .and()
+        .logout()
+        .logoutUrl("/logout")
+        .logoutSuccessUrl("/login")
+        .and()
+        .exceptionHandling()
+        .accessDeniedPage("/errors")
+        .and()
+        .csrf()
+        .disable();
+  }
 
-    @Override
-    public void configure(final WebSecurity web) throws Exception{
-        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "favicon.iso", "/errors/**");//TODO ver cosas con las que no matcheen
-
-    }
-
+  @Override
+  public void configure(final WebSecurity web) throws Exception {
+    web.ignoring()
+        .antMatchers(
+            "/css/**",
+            "/js/**",
+            "/img/**",
+            "/icons/**",
+            "/errors/**"); // TODO ver cosas con las que no matcheen
+  }
 }
