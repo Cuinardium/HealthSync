761badf1b94a8fc2211ce7155eb925be97d6df4c
Franco Bosetti <francobosetti@live.com.ar>
2023-04-28 15:33:28 -0300

    fix: Date formatter now works

--- webapp/src/main/java/ar/edu/itba/paw/webapp/form/AppointmentForm.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/form/AppointmentForm.java
@@ -1,105 +1,115 @@
 package ar.edu.itba.paw.webapp.form;
 
 import ar.edu.itba.paw.webapp.annotations.DateAnnotation;
-
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
-//@DateAnnotation TODO check AMD-64 bit error
-public class AppointmentForm{
+@DateAnnotation
+public class AppointmentForm {
 
-    @Size(min = 1)
-    @Pattern(regexp = "[a-zA-Z ]+")
-    private String name;
+  @Size(min = 1)
+  @Pattern(regexp = "[a-zA-Z ]+")
+  private String name;
 
-    @Size(min = 1)
-    @Pattern(regexp = "[a-zA-Z ]+")
-    private String lastname;
+  @Size(min = 1)
+  @Pattern(regexp = "[a-zA-Z ]+")
+  private String lastname;
 
-    @Size(min = 1)
-    @Pattern(regexp = "[a-zA-Z0-9.+-]+@[a-zA-Z0-9.-]+(.com|.com.ar|.edu.ar)")
-    private String email;
+  @Size(min = 1)
+  @Pattern(regexp = "[a-zA-Z0-9.+-]+@[a-zA-Z0-9.-]+(.com|.com.ar|.edu.ar)")
+  private String email;
 
-    @Size(min = 1)
-    @Pattern(regexp = "[a-zA-Z ]+")
-    private String healthcare;
+  @Size(min = 1)
+  @Pattern(regexp = "[a-zA-Z ]+")
+  private String healthcare;
 
-    private String date;
+  private String date;
 
-    @Size(min = 1, max = 100)
-    private String description;
+  @Size(min = 1, max = 100)
+  private String description;
 
-    private String docEmail;
-    private String address;
-    private String city;
-    private int docId;
+  private String docEmail;
+  private String address;
+  private String city;
+  private int docId;
 
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public String getLastname() {
-        return lastname;
-    }
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
+  public String getLastname() {
+    return lastname;
+  }
 
-    public String getHealthcare() {
-        return healthcare;
-    }
-    public void setHealthcare(String healthcare) {
-        this.healthcare = healthcare;
-    }
+  public void setLastname(String lastname) {
+    this.lastname = lastname;
+  }
 
-    public String getDate() {
-        return date;
-    }
-    public void setDate(String date) {
-        this.date = date;
-    }
+  public String getEmail() {
+    return email;
+  }
 
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
+  public void setEmail(String email) {
+    this.email = email;
+  }
 
-    public String getDocEmail() {
-        return docEmail;
-    }
-    public void setDocEmail(String docEmail) {
-        this.docEmail = docEmail;
-    }
+  public String getHealthcare() {
+    return healthcare;
+  }
 
-    public String getAddress() {
-        return address;
-    }
-    public void setAddress(String address) {
-        this.address = address;
-    }
-    public String getCity() {
-        return city;
-    }
-    public void setCity(String city) {
-        this.city = city;
-    }
+  public void setHealthcare(String healthcare) {
+    this.healthcare = healthcare;
+  }
 
-    public int getDocId() {
-        return docId;
-    }
-    public void setDocId(int docId) {
-        this.docId = docId;
-    }
+  public String getDate() {
+    return date;
+  }
+
+  public void setDate(String date) {
+    this.date = date;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getDocEmail() {
+    return docEmail;
+  }
+
+  public void setDocEmail(String docEmail) {
+    this.docEmail = docEmail;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  public int getDocId() {
+    return docId;
+  }
+
+  public void setDocId(int docId) {
+    this.docId = docId;
+  }
 }
--- webapp/src/main/java/ar/edu/itba/paw/webapp/validators/DateValidator.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/validators/DateValidator.java
@@ -14,8 +14,9 @@ import java.time.format.DateTimeFormatter;
 public class DateValidator implements ConstraintValidator<DateAnnotation, AppointmentForm> {
     @Override
     public boolean isValid(AppointmentForm appointmentForm, ConstraintValidatorContext constraintValidatorContext) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyyTHH:mm");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
         String date= appointmentForm.getDate();
+        date = date.replaceFirst("T", " ");
         if(!date.isEmpty()){
             LocalDateTime dateTime = LocalDateTime.parse(date, formatter);
             return dateTime.isAfter(LocalDateTime.now());
