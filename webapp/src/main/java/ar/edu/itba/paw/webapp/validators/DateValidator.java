662e5fe9887bafb71a98cea3f225c1dd33a39dc0
Franco Bosetti <francobosetti@live.com.ar>
2023-04-28 15:39:31 -0300

    refactor: date formatter now uses ISO standard

--- webapp/src/main/java/ar/edu/itba/paw/webapp/validators/DateValidator.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/validators/DateValidator.java
@@ -1,28 +1,22 @@
 package ar.edu.itba.paw.webapp.validators;
 
-
 import ar.edu.itba.paw.webapp.annotations.DateAnnotation;
 import ar.edu.itba.paw.webapp.form.AppointmentForm;
-
-
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 public class DateValidator implements ConstraintValidator<DateAnnotation, AppointmentForm> {
-    @Override
-    public boolean isValid(AppointmentForm appointmentForm, ConstraintValidatorContext constraintValidatorContext) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-        String date= appointmentForm.getDate();
-        date = date.replaceFirst("T", " ");
-        if(!date.isEmpty()){
-            LocalDateTime dateTime = LocalDateTime.parse(date, formatter);
-            return dateTime.isAfter(LocalDateTime.now());
-        }
-        return false;
-
-
+  @Override
+  public boolean isValid(
+      AppointmentForm appointmentForm, ConstraintValidatorContext constraintValidatorContext) {
+    DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+    String date = appointmentForm.getDate();
+    if (!date.isEmpty()) {
+      LocalDateTime dateTime = LocalDateTime.parse(date, formatter);
+      return dateTime.isAfter(LocalDateTime.now());
     }
+    return false;
+  }
 }
