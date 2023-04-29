e3986e7a7292f6c33fe91177136a2ede2eac142b
Gonzalo Martone <gmartone@itba.edu.ar>
2023-04-29 14:15:07 -0300

    chore: add attending hours table

--- persistence/src/main/resources/schema.sql
+++ persistence/src/main/resources/schema.sql
@@ -31,10 +31,23 @@ CREATE TABLE IF NOT EXISTS doctor_location (
     city_code          INTEGER NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS doctor_attending_hours (
+    attending_hours_id SERIAL PRIMARY KEY,
+    monday             BIGINT NOT NULL,
+    tuesday            BIGINT NOT NULL,
+    wednesday          BIGINT NOT NULL,
+    thursday           BIGINT NOT NULL,
+    friday             BIGINT NOT NULL,
+    saturday           BIGINT NOT NULL,
+    sunday             BIGINT NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS doctor (
     doctor_id       INTEGER PRIMARY KEY,
     specialty_code  INTEGER NOT NULL,
-    FOREIGN KEY (doctor_id) REFERENCES users (user_id)
+    attending_hours_id INTEGER NOT NULL,
+    FOREIGN KEY (doctor_id) REFERENCES users (user_id),
+    FOREIGN KEY (attending_hours_id) REFERENCES attending_hours (attending_hours_id)
 );
 
 CREATE TABLE IF NOT EXISTS location_for_doctor
