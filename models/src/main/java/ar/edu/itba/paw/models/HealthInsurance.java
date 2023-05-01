b1d97b4a17bbd537f1faa1466feff541422bac2f
Gonzalo Martone <gmartone@itba.edu.ar>
2023-04-30 22:40:45 -0300

    chore: add an entry to enum

--- models/src/main/java/ar/edu/itba/paw/models/HealthInsurance.java
+++ models/src/main/java/ar/edu/itba/paw/models/HealthInsurance.java
@@ -4,6 +4,7 @@ public enum HealthInsurance {
 
   // NO CAMBIAR EL ORDEN, SIEMPRE INSERTAR AL FONDO
   // healthInsurance.<healthInsurance> tiene que estar internacionalizado en los i18n
+  NONE("healthInsurance.none"),
   OMINT("healthInsurance.omint"),
   OSDE("healthInsurance.osde"),
   SWISS_MEDICAL("healthInsurance.swiss.medical");
