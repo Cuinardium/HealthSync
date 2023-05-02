21c48988e221160ab8c888a43ca38699b2b7a8ce
Franco Bosetti <francobosetti@live.com.ar>
2023-05-01 23:00:11 -0300

    chore: profile picture in header

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/GlobalControllerAdvice.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/GlobalControllerAdvice.java
@@ -1,13 +1,25 @@
 package ar.edu.itba.paw.webapp.controller;
 
+import ar.edu.itba.paw.interfaces.services.UserService;
+import ar.edu.itba.paw.models.User;
 import ar.edu.itba.paw.webapp.auth.PawAuthUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 @ControllerAdvice
 public class GlobalControllerAdvice {
+
+  private final UserService userService;
+
+  @Autowired
+  public GlobalControllerAdvice(final UserService userService) {
+    this.userService = userService;
+  }
+
   @ModelAttribute("user")
-  public PawAuthUserDetails getCurrentUser() {
-    return PawAuthUserDetails.getCurrentUser();
+  public User getCurrentUser() {
+    long userId = PawAuthUserDetails.getCurrentUserId();
+    return userService.findById(userId).orElse(null);
   }
 }
