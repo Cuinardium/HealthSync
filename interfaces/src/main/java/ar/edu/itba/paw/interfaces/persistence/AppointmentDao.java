975371f6e9e0fc72507c6a3ff7f4af670feb87c4
Santiago Ballerini <sballerini@itba.edu.ar>
2023-05-02 00:00:26 -0300

    feat: add get appointment by status

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/persistence/AppointmentDao.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/persistence/AppointmentDao.java
@@ -1,12 +1,11 @@
 package ar.edu.itba.paw.interfaces.persistence;
 
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Optional;
-
 import ar.edu.itba.paw.models.Appointment;
 import ar.edu.itba.paw.models.AppointmentStatus;
 import ar.edu.itba.paw.models.ThirtyMinuteBlock;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 public interface AppointmentDao {
 
@@ -20,8 +19,12 @@ public interface AppointmentDao {
   Optional<Appointment> getAppointmentById(long appointmentId);
 
   List<Appointment> getAppointmentsForPatient(long patientId);
-  
+
   List<Appointment> getAppointmentsForDoctor(long doctorId);
+
+  List<Appointment> getAppointmentsForPatientByStatus(long patientId, AppointmentStatus status);
+
+  List<Appointment> getAppointmentsForDoctorByStatus(long doctorId, AppointmentStatus status);
 
   void updateAppointmentStatus(long appointmentId, AppointmentStatus status);
 }
