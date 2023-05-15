7e25063fe7dea8225bacfce2f2fde5a4cf60f334
Gonzalo Martone <gmartone@itba.edu.ar>
2023-05-15 14:45:07 -0300

    refactor: rename to ExceptionControllerAdvice

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ExceptionControllerAdvice.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ExceptionControllerAdvice.java
@@ -4,7 +4,6 @@ import ar.edu.itba.paw.webapp.exceptions.AppointmentForbiddenException;
 import ar.edu.itba.paw.webapp.exceptions.AppointmentNotFoundException;
 import ar.edu.itba.paw.webapp.exceptions.ImageNotFoundException;
 import ar.edu.itba.paw.webapp.exceptions.UserNotFoundException;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,7 +11,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
-public class ExceptionController {
+public class ExceptionControllerAdvice {
   @ExceptionHandler(ImageNotFoundException.class)
   @ResponseStatus(code = HttpStatus.NOT_FOUND)
   public ModelAndView noSuchImage(ImageNotFoundException e) {
