0de3429ce237d4019c10311837397827f908535d
Cuinardium <sballerini@itba.edu.ar>
2023-05-15 21:52:11 -0300

    chore: set transactional

--- persistence/src/test/java/ar/edu/itba/paw/persistence/UserDaoImplTest.java
+++ persistence/src/test/java/ar/edu/itba/paw/persistence/UserDaoImplTest.java
@@ -11,10 +11,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.jdbc.JdbcTestUtils;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
+@Rollback
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestConfig.class)
 public class UserDaoImplTest {
--- persistence/src/test/java/ar/edu/itba/paw/persistence/config/TestConfig.java
+++ persistence/src/test/java/ar/edu/itba/paw/persistence/config/TestConfig.java
@@ -6,13 +6,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 import org.springframework.jdbc.datasource.init.DatabasePopulator;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @ComponentScan("ar.edu.itba.paw.persistence")
 @Configuration
+@EnableTransactionManagement
 public class TestConfig {
 
   @Value("classpath:hsqldb.sql")
@@ -50,4 +54,10 @@ public class TestConfig {
 
     return populator;
   }
+
+  @Bean
+  public PlatformTransactionManager transactionManager(final DataSource ds) {
+    return new DataSourceTransactionManager(ds);
+  }
+
 }
