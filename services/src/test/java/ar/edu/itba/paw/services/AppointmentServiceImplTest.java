89787934d11bc5ef9e0cf83b5deda5bb3b31449b
Gonzalo Martone <gmartone@itba.edu.ar>
2023-05-17 12:55:14 -0300

    chore!: appointmentServiceTest boilerplate

--- services/src/test/java/ar/edu/itba/paw/services/AppointmentServiceImplTest.java
+++ services/src/test/java/ar/edu/itba/paw/services/AppointmentServiceImplTest.java
@@ -1,6 +1,7 @@
 package ar.edu.itba.paw.services;
 
 import ar.edu.itba.paw.interfaces.persistence.AppointmentDao;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -14,4 +15,52 @@ public class AppointmentServiceImplTest {
   @Mock private AppointmentDao appointmentDao;
 
   @InjectMocks private AppointmentServiceImpl as;
+
+  @Test
+  public void testCreateAppointment() {}
+
+  @Test
+  public void testCreateAppointmentAlreadyExists() {}
+
+  @Test
+  public void testUpdateAppointment() {}
+
+  @Test
+  public void testUpdateAppointmentDoesNotExist() {}
+
+  @Test
+  public void testGetAppointmentById() {}
+
+  @Test
+  public void testGetAppointmentByIdDoesNotExist() {}
+
+  @Test
+  public void testGetAppointmentsForPatient() {}
+
+  @Test
+  public void testGetAppointmentsForPatientDoesNotExist() {}
+
+  @Test
+  public void testGetAppointmentsForDoctor() {}
+
+  @Test
+  public void testGetAppointmentsForDoctorDoesNotExist() {}
+
+  @Test
+  public void testGetFilteredAppointmentsForPatient() {}
+
+  @Test
+  public void testGetFilteredAppointmentsForPatientDoesNotExist() {}
+
+  @Test
+  public void testGetFilteredAppointmentsForDoctor() {}
+
+  @Test
+  public void testGetFilteredAppointmentsForDoctorDoesNotExist() {}
+
+  @Test
+  public void testGetAvailableHoursForDoctorOnDate() {}
+
+  @Test
+  public void testGetAvailableHoursForDoctorOnRange() {}
 }
