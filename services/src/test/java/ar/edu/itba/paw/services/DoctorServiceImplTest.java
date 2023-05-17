50d5174c3b0da4731438aa30fea39caf85f4e51a
Gonzalo Martone <gmartone@itba.edu.ar>
2023-05-17 12:59:04 -0300

    chore: doctorServiceTest boilerplate

--- services/src/test/java/ar/edu/itba/paw/services/DoctorServiceImplTest.java
+++ services/src/test/java/ar/edu/itba/paw/services/DoctorServiceImplTest.java
@@ -1,6 +1,7 @@
 package ar.edu.itba.paw.services;
 
 import ar.edu.itba.paw.interfaces.persistence.DoctorDao;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -14,4 +15,37 @@ public class DoctorServiceImplTest {
   @Mock private DoctorDao doctorDao;
 
   @InjectMocks private DoctorServiceImpl ds;
+
+  @Test
+  public void testCreateDoctor() {}
+
+  @Test
+  public void testCreateDoctorAlreadyExists() {}
+
+  @Test
+  public void testUpdateDoctor() {}
+
+  @Test
+  public void testUpdateDoctorDoesNotExist() {}
+
+  @Test
+  public void testGetDoctorById() {}
+
+  @Test
+  public void testGetDoctorByIdDoesNotExist() {}
+
+  @Test
+  public void testGetFilteredDoctors() {}
+
+  @Test
+  public void testGetDoctors() {}
+
+  @Test
+  public void testGetUsedSpecialties() {}
+
+  @Test
+  public void testGetUsedHealthInsurances() {}
+
+  @Test
+  public void testGetUsedCities() {}
 }
