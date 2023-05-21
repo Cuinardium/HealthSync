f618337a95009d2f2a27f5949780fca793045f4d
Gonzalo Martone <gmartone@itba.edu.ar>
2023-05-21 16:12:13 -0300

    chore: add annotations to user model

--- models/src/main/java/ar/edu/itba/paw/models/User.java
+++ models/src/main/java/ar/edu/itba/paw/models/User.java
@@ -1,28 +1,45 @@
 package ar.edu.itba.paw.models;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "users") // Si la clase no tiene el mismo nombre que la DB table
 public class User {
   // TODO: agregar restricciones (solo las usa si levanto la DB con Hibernate)
-  // @Id
-  // @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_userid_seq")
-  // @SequenceGenerator(sequenceName = "users_userid_seq", name = "users_userid_seq", allocationSize
-  // = 1)
-  // @Column(name = "userid")
-  private final Long id; // uso Long para q si es nulo inserte
-  private final Long profilePictureId;
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_userid_seq")
+  @SequenceGenerator(
+    sequenceName = "users_userid_seq",
+    name = "users_userid_seq",
+    allocationSize = 1
+  )
+  @Column(name = "user_id")
+  private Long id; // uso Long para q si es nulo inserte
 
-  // @Column(length = 100, unique = true, nullable = false)
-  private final String email;
+  @Column(length = 100, unique = true, nullable = false)
+  private String email;
 
-  // @Column(length = 60, nullable = false)
+  @Column(length = 100, nullable = false)
   private String password;
 
-  private final String firstName;
-  private final String lastName;
+  @Column(name = "first_name", length = 100, nullable = false)
+  private String firstName;
+
+  @Column(name = "last_name", length = 100, nullable = false)
+  private String lastName;
+
+  @Column(name = "profile_picture_id")
+  private Long profilePictureId;
+
+  /* package */ User() {
+    // Solo para hibernate
+  }
 
   public User(
       Long id,
@@ -44,6 +61,10 @@ public class User {
     return email;
   }
 
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
   public String getPassword() {
     return password;
   }
@@ -60,12 +81,24 @@ public class User {
     return firstName;
   }
 
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
   public String getLastName() {
     return lastName;
   }
 
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
   public Long getProfilePictureId() {
     return profilePictureId;
+  }
+
+  public void setProfilePictureId(Long profilePictureId) {
+    this.profilePictureId = profilePictureId;
   }
 
   @Override
