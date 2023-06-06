e25ab50d80b0fd1236fab73041de88b5efb50ca5
Gonzalo Martone <gmartone@itba.edu.ar>
2023-06-06 20:33:49 -0300

    fix: somehow now the tests pass

--- models/src/main/java/ar/edu/itba/paw/models/AttendingHours.java
+++ models/src/main/java/ar/edu/itba/paw/models/AttendingHours.java
@@ -162,7 +162,7 @@ public class AttendingHours {
     public int hashCode() {
       final int prime = 31;
       int result = 1;
-      result = prime * result + ((doctorId == null) ? 0 : doctorId.hashCode());
+      // result = prime * result + ((doctorId == null) ? 0 : doctorId.hashCode());
       result = prime * result + ((day == null) ? 0 : day.hashCode());
       result = prime * result + ((hourBlock == null) ? 0 : hourBlock.hashCode());
       return result;
