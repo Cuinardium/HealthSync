ad1d3cdaefb33587744513a7004a51ba29720290
Gonzalo Martone <gmartone@itba.edu.ar>
2023-06-06 19:37:20 -0300

    chore: agregar mas obras sociales

--- models/src/main/java/ar/edu/itba/paw/models/HealthInsurance.java
+++ models/src/main/java/ar/edu/itba/paw/models/HealthInsurance.java
@@ -4,12 +4,35 @@ import java.util.*;
 
 public enum HealthInsurance {
 
+  // las obtuve consultando www.miobrasocial.com.ar
   // NO CAMBIAR EL ORDEN, SIEMPRE INSERTAR AL FONDO
   // healthInsurance.<healthInsurance> tiene que estar internacionalizado en los i18n
   NONE("healthInsurance.none"),
   OMINT("healthInsurance.omint"),
   OSDE("healthInsurance.osde"),
-  SWISS_MEDICAL("healthInsurance.swiss.medical");
+  SWISS_MEDICAL("healthInsurance.swiss.medical"),
+  GALENO("healthInsurance.galeno"),
+  MEDIFE("healthInsurance.medife"),
+  MEDICUS("healthInsurance.medicus"),
+  ACCORD("healthInsurance.accord"),
+  HOSPITAL_ITALIANO("healthInsurance.hospital.italiano"),
+  HOSPITAL_ALEMAN("healthInsurance.hospital.aleman"),
+  HOSPITAL_BRITANICO("healthInsurance.hospital.britanico"),
+  PREVENCION_SALUD("healthInsurance.prevencion.salud"),
+  AVALIAN("healthInsurance.avalian"),
+  WILLIAM_HOPE("healthInsurance.william.hope"),
+  PRE_MEDIC("healthInsurance.pre.medic"),
+  OSFE("healthInsurance.osfe"),
+  OSPACA("healthInsurance.ospaca"),
+  ANDAR("healthInsurance.andar"),
+  LUIS_PASTEUR("healthInsurance.luis.pasteur"),
+  UNION_PERSONAL("healthInsurance.union.personal"),
+  JERARQUICOS("healthInsurance.jerarquicos"),
+  OSDEPYM("healthInsurance.osdepym"),
+  OSPE("healthInsurance.ospe"),
+  OSPLAD("healthInsurance.osplad"),
+  OSSEG("healthInsurance.osseg"),
+  SANCOR_SALUD("healthInsurance.sancor.salud");
 
   private final String messageID;
 
