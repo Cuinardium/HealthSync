0af96a718b270ef2c2ca82d976509c595c5f7b08
Juan Cruz Bafico <jbafico@itba.edu.ar>
2023-06-26 10:16:57 -0300

    fix: notification now deletes

--- persistence/src/main/java/ar/edu/itba/paw/persistence/NotificationDaoJpa.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/NotificationDaoJpa.java
@@ -6,12 +6,9 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 import javax.persistence.TypedQuery;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Repository
 public class NotificationDaoJpa implements NotificationDao {
@@ -27,7 +24,7 @@ public class NotificationDaoJpa implements NotificationDao {
 
     @Override
     public void deleteNotification(Notification notification) {
-        em.detach(notification);
+        em.remove(notification);
     }
 
     @Override
