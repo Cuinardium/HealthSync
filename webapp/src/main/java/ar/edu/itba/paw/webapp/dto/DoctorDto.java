0c4811b5d2340e5d6117dbf6f678c4db9cb05f03
Gonzalo Martone <gmartone@itba.edu.ar>
2023-12-26 16:18:35 -0300

    feat: add more fields to doctor response

--- webapp/src/main/java/ar/edu/itba/paw/webapp/dto/DoctorDto.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/dto/DoctorDto.java
@@ -9,15 +9,27 @@ import javax.ws.rs.core.UriInfo;
 public class DoctorDto {
   // TODO: add fields
   private String firstName;
+  private String lastName;
+  private String address;
+  private String city;
+
+  private URI healthInsurances;
+  private URI attendingHours;
+  private URI image;
   private URI self;
 
   public static DoctorDto fromDoctor(final UriInfo uri, final Doctor doctor) {
     final DoctorDto dto = new DoctorDto();
     // TODO: more fields
     dto.firstName = doctor.getFirstName();
+    dto.lastName = doctor.getLastName();
+    dto.address = doctor.getAddress();
+    dto.city = doctor.getCity();
+    // TODO: doctor get Image id para armar el url
+    // dto.image =
+    // uri.getBaseUriBuilder().path("/images").path(String.valueOf(doctor.getImageId())).build();
     dto.self =
         uri.getBaseUriBuilder().path("/doctors").path(String.valueOf(doctor.getId())).build();
-
     return dto;
   }
 
@@ -39,5 +51,37 @@ public class DoctorDto {
 
   public void setSelf(URI self) {
     this.self = self;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public URI getImage() {
+    return image;
+  }
+
+  public void setImage(URI image) {
+    this.image = image;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
   }
 }
