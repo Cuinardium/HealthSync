b45b0597817e1de6898988c98b3a78f61c6ce487
Gonzalo Martone <gmartone@itba.edu.ar>
2024-01-03 21:45:56 -0300

    fix: validations for NotNull on patient fields

--- webapp/src/main/java/ar/edu/itba/paw/webapp/form/PatientRegisterForm.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/form/PatientRegisterForm.java
@@ -2,9 +2,11 @@ package ar.edu.itba.paw.webapp.form;
 
 import ar.edu.itba.paw.models.HealthInsurance;
 import ar.edu.itba.paw.webapp.annotations.ExistsInEnum;
+import javax.validation.constraints.NotNull;
 
 public class PatientRegisterForm extends UserRegisterForm {
 
+  @NotNull
   @ExistsInEnum(enumClass = HealthInsurance.class)
   private Integer healthInsuranceCode = -1;
 
