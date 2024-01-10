fb741734b4e116a989cde5b8b926dfdfce023aaf
Franco Bosetti <francobosetti@live.com.ar>
2024-01-10 01:17:17 -0300

    chore: replace deprecated methods

--- webapp/src/main/java/ar/edu/itba/paw/webapp/auth/JwtUtil.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/auth/JwtUtil.java
@@ -1,77 +1,76 @@
 package ar.edu.itba.paw.webapp.auth;
 
 import ar.edu.itba.paw.models.User;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtParser;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.stereotype.Component;
-
-import javax.servlet.http.HttpServletRequest;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.Date;
 import java.util.function.Function;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
 
 @Component
 public class JwtUtil {
 
-    private final String SECRET_KEY = "CFGJ"; //Cuini Franco Gonza Juan
-    private static final long accessTokenValidity= 24 * 60 * 60 * 1000;
+  private static final long accessTokenValidity = 24 * 60 * 60 * 1000;
+  private final JwtParserBuilder jwtParserBuilder;
+  private final Key SECRET_KEY;
+  private final String TOKEN_HEADER = "Authorization";
+  private final String TOKEN_PREFIX = "Bearer ";
 
-    private final JwtParser jwtParser;
+  public JwtUtil() {
+    final String secretKeyBase = "CFGJ"; // Cuini Franco Gonza Juan
+    this.SECRET_KEY = Keys.hmacShaKeyFor(secretKeyBase.getBytes(StandardCharsets.UTF_8));
+    this.jwtParserBuilder = Jwts.parserBuilder().setSigningKey(this.SECRET_KEY);
+  }
 
-    private final String TOKEN_HEADER = "Authorization";
-    private final String TOKEN_PREFIX = "Bearer ";
+  public String generateAccessToken(User user) {
+    return Jwts.builder()
+        .setSubject(String.format("%s,%s", user.getId(), user.getEmail()))
+        .setIssuer("paw-2023a-02")
+        .setIssuedAt(new Date())
+        .setExpiration(new Date(System.currentTimeMillis() + accessTokenValidity))
+        .signWith(SECRET_KEY, SignatureAlgorithm.HS512)
+        .compact();
+  }
 
-    public JwtUtil(){
-        this.jwtParser = Jwts.parser().setSigningKey(SECRET_KEY);
+  private Claims parseJwtClaims(String token) {
+    return jwtParserBuilder.build().parseClaimsJws(token).getBody();
+  }
+
+  public Claims resolveClaims(HttpServletRequest req) {
+
+    String token = resolveToken(req);
+    if (token != null) {
+      return parseJwtClaims(token);
     }
+    return null;
+  }
 
-    public String generateAccessToken(User user){
-        return Jwts.builder()
-                .setSubject(String.format("%s,%s", user.getId(), user.getEmail()))
-                .setIssuer("paw-2023a-02")
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + accessTokenValidity))
-                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
-                .compact();
+  public String resolveToken(HttpServletRequest request) {
+
+    String bearerToken = request.getHeader(TOKEN_HEADER);
+    if (bearerToken != null && bearerToken.startsWith(TOKEN_PREFIX)) {
+      return bearerToken.substring(TOKEN_PREFIX.length());
     }
+    return null;
+  }
 
-    private Claims parseJwtClaims(String token) {
-        return jwtParser.parseClaimsJws(token).getBody();
-    }
+  public boolean validateClaims(Claims claims) {
+    return claims.getExpiration().after(new Date());
+  }
 
-    public Claims resolveClaims(HttpServletRequest req) {
+  public boolean isTokenRefresh(String token) {
+    return getClaimFromToken(token, c -> c.get("refresh", Boolean.class) != null);
+  }
 
-        String token = resolveToken(req);
-        if (token != null) {
-            return parseJwtClaims(token);
-        }
-        return null;
+  public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+    final Claims claims = parseClaims(token);
+    return claimsResolver.apply(claims);
+  }
 
-    }
-
-    public String resolveToken(HttpServletRequest request) {
-
-        String bearerToken = request.getHeader(TOKEN_HEADER);
-        if (bearerToken != null && bearerToken.startsWith(TOKEN_PREFIX)) {
-            return bearerToken.substring(TOKEN_PREFIX.length());
-        }
-        return null;
-    }
-
-    public boolean validateClaims(Claims claims){
-        return claims.getExpiration().after(new Date());
-    }
-
-    public boolean isTokenRefresh(String token) {
-        return getClaimFromToken(token, c -> c.get("refresh", Boolean.class) != null);
-    }
-    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
-        final Claims claims = parseClaims(token);
-        return claimsResolver.apply(claims);
-    }
-
-    private Claims parseClaims(String token) {
-        return Jwts.parserBuilder().setSigningKey(SECRET_KEY).build().parseClaimsJws(token).getBody();
-    }
+  private Claims parseClaims(String token) {
+    return Jwts.parserBuilder().setSigningKey(SECRET_KEY).build().parseClaimsJws(token).getBody();
+  }
 }
