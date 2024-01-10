a919d87ea2a7d6a3ae29e23c98e07065d98e4532
Franco Bosetti <francobosetti@live.com.ar>
2024-01-10 20:18:22 -0300

    chore: not null validation auth form

--- webapp/src/main/java/ar/edu/itba/paw/webapp/form/AuthForm.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/form/AuthForm.java
@@ -1,31 +1,34 @@
 package ar.edu.itba.paw.webapp.form;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class AuthForm {
 
-    @Size(min = 1)
-    @Pattern(regexp = "[a-zA-Z0-9.+-ñÑ]+@[a-zA-Z0-9.-]+(.com|.com.ar|.edu.ar)")
-    private String email;
+  @Size(min = 1)
+  @NotNull
+  @Pattern(regexp = "[a-zA-Z0-9.+-ñÑ]+@[a-zA-Z0-9.-]+(.com|.com.ar|.edu.ar)")
+  private String email;
 
-    @Size(min = 4)
-    @Pattern(regexp = "[a-zA-Z0-9]+")
-    private String password;
+  @Size(min = 4)
+  @NotNull
+  @Pattern(regexp = "[a-zA-Z0-9]+")
+  private String password;
 
-    public String getEmail() {
-        return email;
-    }
+  public String getEmail() {
+    return email;
+  }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+  public void setEmail(String email) {
+    this.email = email;
+  }
 
-    public String getPassword() {
-        return password;
-    }
+  public String getPassword() {
+    return password;
+  }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+  public void setPassword(String password) {
+    this.password = password;
+  }
 }
