b9d2b835b4f5125b3836d84905716cd078a6e7a6
Santiago Ballerini <sballerini@itba.edu.ar>
2024-07-01 17:54:46 -0300

    feat: implement single indication query

--- interfaces-services/src/main/java/ar/edu/itba/paw/interfaces/services/IndicationService.java
+++ interfaces-services/src/main/java/ar/edu/itba/paw/interfaces/services/IndicationService.java
@@ -5,6 +5,8 @@ import ar.edu.itba.paw.models.File;
 import ar.edu.itba.paw.models.Indication;
 import ar.edu.itba.paw.models.Page;
 
+import java.util.Optional;
+
 public interface IndicationService {
 
     // =============== Inserts ===============
@@ -12,8 +14,11 @@ public interface IndicationService {
     public Indication createIndication(long appointmentId, long userId, String description, File file)
             throws UserNotFoundException, AppointmentNotFoundException;
 
+
     // =============== Queries ===============
 
     public Page<Indication> getIndicationsForAppointment(long appointmentId, Integer page, Integer pageSize)
             throws AppointmentNotFoundException;
+
+    public Optional<Indication> getIndication(long indicationId);
 }
