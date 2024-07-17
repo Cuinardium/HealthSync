45530d26368e9f742fa8ff2c6a54e968d05edf11
Santiago Ballerini <sballerini@itba.edu.ar>
2024-07-17 18:35:49 -0300

    feat: add i18n for healthinsurance responses

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HealthInsuranceController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HealthInsuranceController.java
@@ -1,19 +1,22 @@
 package ar.edu.itba.paw.webapp.controller;
 
 import ar.edu.itba.paw.models.HealthInsurance;
+import ar.edu.itba.paw.webapp.dto.ErrorDto;
 import ar.edu.itba.paw.webapp.dto.HealthInsuranceDto;
-import ar.edu.itba.paw.webapp.exceptions.UserNotFoundException;
+import ar.edu.itba.paw.webapp.mediaType.VndType;
+import ar.edu.itba.paw.webapp.utils.LocaleUtil;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.*;
-
-import ar.edu.itba.paw.webapp.mediaType.VndType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 
 @Path("healthinsurances")
@@ -21,29 +24,52 @@ import org.springframework.stereotype.Component;
 public class HealthInsuranceController {
   private static final Logger LOGGER = LoggerFactory.getLogger(HealthInsuranceController.class);
 
+  private final MessageSource messageSource;
+
   @Context private UriInfo uriInfo;
 
-  // TODO: paginate?
+  @Autowired
+  public HealthInsuranceController(final MessageSource messageSource) {
+    this.messageSource = messageSource;
+  }
+
   @GET
   @Produces(VndType.APPLICATION_HEALTH_INSURANCE_LIST)
   public Response listHealthInsurances() {
     List<HealthInsurance> healthInsuranceList = Arrays.asList(HealthInsurance.values());
 
     final List<HealthInsuranceDto> dtoList =
-        HealthInsuranceDto.fromHealthInsuranceList(uriInfo, healthInsuranceList);
+        healthInsuranceList.stream()
+            .map(
+                healthInsurance ->
+                    HealthInsuranceDto.fromHealthInsurance(uriInfo, messageSource, healthInsurance))
+            .collect(Collectors.toList());
+
     return Response.ok(new GenericEntity<List<HealthInsuranceDto>>(dtoList) {}).build();
   }
 
   @GET
   @Produces(VndType.APPLICATION_HEALTH_INSURANCE)
-  @Path("/{id}")
-  public Response getHealthInsurance(@PathParam("id") final int id) throws UserNotFoundException {
+  @Path("/{id:\\d+}")
+  public Response getHealthInsurance(@PathParam("id") final int id) {
+
     HealthInsurance[] healthInsurances = HealthInsurance.values();
     if (id < 0 || id >= healthInsurances.length) {
-      return Response.status(Response.Status.NOT_FOUND).build();
+      String message =
+          messageSource.getMessage(
+              "error.healthInsuranceNotFound", null, LocaleUtil.getCurrentRequestLocale());
+
+      return Response.status(Response.Status.NOT_FOUND)
+          .entity(ErrorDto.fromMessage(message))
+          .build();
     }
+
     HealthInsurance healthInsurance = healthInsurances[id];
+
     LOGGER.debug("returning healthInsurance with id {}", id);
-    return Response.ok(HealthInsuranceDto.fromHealthInsurance(uriInfo, healthInsurance)).build();
+
+    return Response.ok(
+            HealthInsuranceDto.fromHealthInsurance(uriInfo, messageSource, healthInsurance))
+        .build();
   }
 }
