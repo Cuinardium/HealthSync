45afdbcf5948d18c688a705ae6dcdb6c6fa41f83
Juan Cruz Bafico <jbafico@itba.edu.ar>
2024-07-18 10:48:11 -0300

    fix: fixed header appearance

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,85 +2,144 @@ import React from 'react';
 import { Navbar, Nav, NavDropdown, Button, Image } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 
+import logo from '../img/logo.svg'
+import loggedUserImg from '../img/doctorDefault.png'
+
+import 'bootstrap/dist/css/bootstrap.css';
+import '../css/main.css'
+import '../css/header.css'
+
+// Define the API URLs or other variables
+const homeUrl= '/';
+const dashboardUrl= '/doctor-dashboard';
+const myAppointmentsUrl= '/my-appointments';
+const doctorRegisterUrl= '/doctor-register';
+const patientRegisterUrl= '/patient-register';
+const doctorProfileUrl= '/doctor-profile';
+const patientProfileUrl= '/patient-profile';
+const loginUrl= '/login';
+const logoutUrl= '/logout';
+
 const Header = ({ user, hasNotifications, isDoctor }) => {
     const { t } = useTranslation();
 
-    const urls = {
-        mainCss: '/css/main.css',
-        headerCss: '/css/header.css',
-        homeUrl: '/',
-        dashboardUrl: '/doctor-dashboard',
-        myAppointmentsUrl: '/my-appointments',
-        doctorRegisterUrl: '/doctor-register',
-        patientRegisterUrl: '/patient-register',
-        doctorProfileUrl: '/doctor-profile',
-        patientProfileUrl: '/patient-profile',
-        loginUrl: '/login',
-        logoutUrl: '/logout',
-        logo: '/img/logo.svg'
-    };
-
     return (
-            <Navbar bg="light" expand="lg" className="border-bottom">
-                <Navbar.Brand href={urls.homeUrl} className="d-flex align-items-center">
-                    <img src={urls.logo} alt="HealthSync logo" className="logo" />
+            <header className="border-bottom">
+                <div className="head">
+                <a href={homeUrl} className="d-flex align-items-center">
+                    <img src={logo} alt="HealthSync logo" className="logo" />
                     <div className="health title">Health</div>
                     <div className="sync title">Sync</div>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href={urls.dashboardUrl}>
+                </a>
+                    <div className="buttons">
+                        <a href={dashboardUrl} className="headerLink">
                             {t('home.checkDoctor')}
-                            <i className="fa-solid fa-user-doctor ms-2"></i>
-                        </Nav.Link>
+                            <i className="fa-solid fa-user-doctor"></i>
+                        </a>
                         {user && (
                             <>
-                                <Nav.Link href={urls.myAppointmentsUrl}>
+                                <a href={myAppointmentsUrl} className="headerLink">
                                     {t('home.myAppointments')}
-                                    <i className="fa-solid fa-calendar-check ms-2"></i>
-                                </Nav.Link>
-                                {hasNotifications && <i className="fa-solid fa-circle notification ms-2"></i>}
+                                    <i className="fa-solid fa-calendar-check"></i>
+                                </a>
+                                {hasNotifications && <i className="fa-solid fa-circle notification"></i>}
                             </>
                         )}
-                    </Nav>
+                    </div>
                     {user ? (
-                        <Nav>
-                            <NavDropdown
-                                title={
-                                    <div className="d-flex align-items-center">
-                                        <i className={`fa-solid ${isDoctor ? 'fa-user-nurse' : 'fa-user'} iconIdentifier me-2`}></i>
-                                        <Image src={urls.loggedUserImg} alt={t('user.alt.loggedUserImg')} roundedCircle width="40" height="40" />
-                                    </div>
-                                }
-                                id="basic-nav-dropdown"
-                            >
-                                <NavDropdown.Item href={isDoctor ? urls.doctorProfileUrl : urls.patientProfileUrl}>
-                                    {t('header.profile')}
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href={urls.logoutUrl}>
-                                    {t('header.logout')}
-                                </NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                    ) : (
-                        <Nav>
-                            <Button href={urls.loginUrl} variant="primary" className="me-2">
+                        <nav className="navbar navbar-expand-lg">
+                            <div className="container-fluid">
+                                {isDoctor ? (
+                                    <>
+                                        <i className="fa-solid fa-user-nurse iconIdentifier"></i>
+                                    </>
+                                ) : (
+                                    <>
+                                        <i className="fa-solid fa-user iconIdentifier"></i>
+                                    </>
+                                )}
+                                <div id="profileDropdown">
+                                    <ul className="navbar-nav">
+                                        <li className="nav-item dropdown">
+                                            <a
+                                                className="nav-link dropdown-toggle"
+                                                href="#"
+                                                role="button"
+                                                data-bs-toggle="dropdown"
+                                                aria-haspopup="true"
+                                                aria-expanded="false"
+                                                data-toggle="tooltip"
+                                                data-placement="top"
+                                            >
+                                                <img
+                                                    src={loggedUserImg}
+                                                    alt={t('user.alt.loggedUserImg')}
+                                                    width="40"
+                                                    height="40"
+                                                    className="rounded-circle"
+                                                />
+                                            </a>
+                                            <ul className="dropdown-menu">
+                                                {isDoctor ? (
+                                                    <li>
+                                                        <a className="dropdown-item icon" href={doctorProfileUrl}>
+                                                            {t('header.profile')}
+                                                        </a>
+                                                    </li>
+                                                ) : (
+                                                    <li>
+                                                        <a className="dropdown-item" href={patientProfileUrl}>
+                                                            {t('header.profile')}
+                                                        </a>
+                                                    </li>
+                                                )}
+                                                <li>
+                                                    <a className="dropdown-item" href={logoutUrl}>
+                                                        {t('header.logout')}
+                                                    </a>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </nav>
+                    ): (
+                        <div className="buttons">
+                            <a href={loginUrl} className="btn btn-primary" role="button">
                                 {t('login.title')}
-                            </Button>
-                            <NavDropdown title={t('register.title')} id="basic-nav-dropdown">
-                                <NavDropdown.Item href={urls.doctorRegisterUrl}>
-                                    {t('header.iAmDoctor')}
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href={urls.patientRegisterUrl}>
-                                    {t('header.iAmPatient')}
-                                </NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                    )}
-                </Navbar.Collapse>
-            </Navbar>
-    );
-};
+                            </a>
 
-export default Header;
+                            <nav className="navbar navbar-expand-lg">
+                                <div className="container-fluid">
+                                    <div id="navbarNavDropdown">
+                                        <ul className="navbar-nav">
+                                            <li className="nav-item dropdown">
+                                                <button className="btn dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                                    {t('register.title')}
+                                                </button>
+                                                <ul className="dropdown-menu">
+                                                    <li>
+                                                        <a className="dropdown-item" href={doctorRegisterUrl}>
+                                                            {t('header.iAmDoctor')}
+                                                        </a>
+                                                    </li>
+                                                    <li>
+                                                        <a className="dropdown-item" href={patientRegisterUrl}>
+                                                            {t('header.iAmPatient')}
+                                                        </a>
+                                                    </li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </nav>
+                        </div>
+                    )}
+                </div>
+            </header>
+                    );
+                    };
+
+                    export default Header;
