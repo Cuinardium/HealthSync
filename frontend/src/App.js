20cb3a51cba73f300ff599a2f6c2f18f370d9499
Juan Cruz Bafico <jbafico@itba.edu.ar>
2024-07-18 10:58:51 -0300

    feat: enabled dropdown menus

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,9 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import HomePage from "./home/HomePage";
 import {HelmetProvider} from "react-helmet-async";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 function App() {
     const helmetContext = {};
 
