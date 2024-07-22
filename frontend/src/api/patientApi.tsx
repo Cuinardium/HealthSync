548a06638af9c7cca2d39321f7464c7d8082897f
Juan Cruz Bafico <jbafico@itba.edu.ar>
2024-07-22 14:44:08 -0300

    feat: patientApi get and create

--- frontend/src/api/patientApi.tsx
+++ frontend/src/api/patientApi.tsx
@@ -1,3 +1,16 @@
 import api from "./api";
+import {Patient} from "../models/Patient";
 
 const patientEndpoint = '/patients'
+
+
+//TODO revisar tema headers
+export async function createPatient(patient: Patient): Promise<Patient>{
+    const response = await api.post(patientEndpoint, Patient.toJson(patient));
+    return response.data;
+}
+
+export async function getPatientById(id : String): Promise<Patient>{
+    const response = await api.get(`${patientEndpoint}/${id}`);
+    return Patient.fromJson(response.data)
+}
