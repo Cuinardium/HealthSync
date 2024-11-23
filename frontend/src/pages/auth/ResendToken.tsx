78a50cc624c5ece2f4c00781f4430f3b49d03e3a
Santiago Ballerini <sballerini@itba.edu.ar>
2024-11-23 20:26:30 -0300

    feat: add token resend

--- frontend/src/pages/auth/ResendToken.tsx
+++ frontend/src/pages/auth/ResendToken.tsx
@@ -1,6 +1,53 @@
+import { AxiosError } from "axios";
+import { t } from "i18next";
+import { useState } from "react";
+import { Button, Card, Container, Form } from "react-bootstrap";
+import { resendVerificationToken } from "../../api/token/tokenApi";
 
 const ResendToken = () => {
-  return (<div>WIP</div>)
-}
+  const [email, setEmail] = useState<string>("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      await resendVerificationToken(email);
+      alert("verfication to email sent");
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        const body = JSON.stringify(error.response?.data);
+        alert(`Error: ${body}`);
+      }
+    }
+  };
+
+  return (
+    <Container>
+      <h1>Resend Token</h1>
+      <Card>
+        <Card.Body>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="formRow" controlId="formEmail">
+              <div className="form-check">
+                <Form.Label className="label">{t("form.email")}</Form.Label>
+                <Form.Control
+                  className="form-input"
+                  type="text"
+                  name="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder={t("form.email_hint")}
+                />
+              </div>
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Resend token
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+};
 
 export default ResendToken;
