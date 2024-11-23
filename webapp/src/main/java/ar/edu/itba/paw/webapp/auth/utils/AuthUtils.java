cac7747ecc00804c6a85092fadccc41edce970fb
Santiago Ballerini <sballerini@itba.edu.ar>
2024-11-23 20:53:07 -0300

    delete: remove authutils

--- webapp/src/main/java/ar/edu/itba/paw/webapp/auth/utils/AuthUtils.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.itba.paw.webapp.auth.utils;
-
-import javax.servlet.http.HttpServletRequest;
-
-public class AuthUtils {
-    public static String getBaseUrl(HttpServletRequest request){
-        return  request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+request.getContextPath()+"/api";
-    }
-}
