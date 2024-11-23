20d8f077fd45dbc9140456f9128d262975829c7f
Santiago Ballerini <sballerini@itba.edu.ar>
2024-11-23 20:52:44 -0300

    refactor: remove verification logic in basic filter

--- webapp/src/main/java/ar/edu/itba/paw/webapp/auth/BasicAuthFilter.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/auth/BasicAuthFilter.java
@@ -4,13 +4,10 @@ import ar.edu.itba.paw.interfaces.services.TokenService;
 import ar.edu.itba.paw.interfaces.services.UserService;
 import ar.edu.itba.paw.interfaces.services.exceptions.TokenNotFoundException;
 import ar.edu.itba.paw.models.User;
-import ar.edu.itba.paw.models.VerificationToken;
-import ar.edu.itba.paw.webapp.auth.utils.AuthUtils;
 import ar.edu.itba.paw.webapp.exceptions.AuthenticationErrorException;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
-import java.util.Optional;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -25,41 +22,34 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.authentication.www.NonceExpiredException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
 public class BasicAuthFilter extends OncePerRequestFilter {
-  @Autowired private JwtUtil jwtUtil;
-
-  @Autowired private AuthenticationEntryPoint authenticationEntryPoint;
-
-  @Autowired private UserService userService;
-
-  @Autowired private TokenService tokenService;
-
-  @Autowired private PawUserDetailsService userDetailsService;
-
-  @Autowired private AuthenticationManager authenticationManager;
-
   private static final int EMAIL_INDEX = 0;
   private static final int PASSWORD_INDEX = 1;
-  private static final int VERIFICATION_TOKEN_INDEX = 1;
 
-  private static final String TOKEN_HEADER = "X-JWT";
-  private static final String REFRESH_TOKEN_HEADER = "X-Refresh";
   private static final String BASIC_PREFIX = "Basic ";
+  @Autowired private JwtUtil jwtUtil;
+  @Autowired private AuthenticationEntryPoint authenticationEntryPoint;
+  @Autowired private UserService userService;
+  @Autowired private TokenService tokenService;
+  @Autowired private PawUserDetailsService userDetailsService;
+  @Autowired private AuthenticationManager authenticationManager;
 
   @Override
   protected void doFilterInternal(
       HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
+
     final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+
     if (header == null || !header.startsWith(BASIC_PREFIX)) {
       filterChain.doFilter(request, response);
       return;
     }
+
     Authentication authentication;
     try {
       String[] credentials = getDecodedHeader(header);
@@ -71,25 +61,15 @@ public class BasicAuthFilter extends OncePerRequestFilter {
                   () ->
                       new UsernameNotFoundException(
                           "No user for email " + credentials[EMAIL_INDEX]));
-      Optional<VerificationToken> token = tokenService.getUserToken(user);
-      if (token.isPresent()
-          && token.get().getToken().equals(credentials[VERIFICATION_TOKEN_INDEX])) {
-        if (token.get().isExpired()) throw new NonceExpiredException("Token expired");
 
-        response.addHeader(
-            TOKEN_HEADER, jwtUtil.generateAccessToken(user, AuthUtils.getBaseUrl(request)));
-        response.addHeader(REFRESH_TOKEN_HEADER, jwtUtil.generateRefreshToken(user));
-        authentication = getAuthWithVerifToken(user);
-      } else {
-        authentication =
-            authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                    credentials[EMAIL_INDEX], credentials[PASSWORD_INDEX]));
+      authentication =
+          authenticationManager.authenticate(
+              new UsernamePasswordAuthenticationToken(
+                  credentials[EMAIL_INDEX], credentials[PASSWORD_INDEX]));
 
-        response.addHeader(
-            TOKEN_HEADER, jwtUtil.generateAccessToken(user, AuthUtils.getBaseUrl(request)));
-        response.addHeader(REFRESH_TOKEN_HEADER, jwtUtil.generateRefreshToken(user));
-      }
+      response.addHeader(JwtUtil.TOKEN_HEADER, jwtUtil.generateAccessToken(user));
+      response.addHeader(JwtUtil.REFRESH_TOKEN_HEADER, jwtUtil.generateRefreshToken(user));
+
     } catch (AuthenticationException e) {
       SecurityContextHolder.clearContext();
       authenticationEntryPoint.commence(request, response, e);
@@ -98,19 +78,6 @@ public class BasicAuthFilter extends OncePerRequestFilter {
 
     SecurityContextHolder.getContext().setAuthentication(authentication);
     filterChain.doFilter(request, response);
-  }
-
-  private Authentication getAuthWithVerifToken(User user) throws DisabledException {
-    try {
-      tokenService.deleteUserToken(user);
-    } catch (TokenNotFoundException e) {
-      throw new DisabledException("Token not found");
-    }
-
-    PawAuthUserDetails userDetails =
-        (PawAuthUserDetails) userDetailsService.loadUserByUsername(user.getEmail());
-    return new UsernamePasswordAuthenticationToken(
-        userDetails, userDetails.getPassword(), userDetails.getAuthorities());
   }
 
   private String[] getDecodedHeader(String header) {
