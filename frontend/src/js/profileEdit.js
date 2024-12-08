c2ab87d74f199fe4d34a8ad8d6eee64deb2ca059
Santiago Ballerini <sballerini@itba.edu.ar>
2024-12-07 23:38:37 -0300

    delete: remove unused js files

--- frontend/src/js/profileEdit.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('input[type="file"]').addEventListener('change', function() {
-        if (this.files && this.files[0]) {
-            var img = document.querySelector('#imgPreview'); // $('img')[0]
-            img.src = URL.createObjectURL(this.files[0]); // set src to file url
-        }
-    });
-});
