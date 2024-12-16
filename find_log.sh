ace065d3ba7d9f544b6670237d995828b2d942b2
Santiago Ballerini <sballerini@itba.edu.ar>
2024-12-16 19:09:58 -0300

    chore: remove unused scripts

--- find_log.sh
+++
@@ -1,8 +0,0 @@
-url=http://old-pawserver.it.itba.edu.ar/logs/localhost.$(date +%Y-%m-%d).log
-group="paw-2023a-02"
-separator="================================================================"
-
-echo -e "getting logs from ${url}, saving to /tmp/${group}.log"
-curl ${url} --output /tmp/${group}.log --silent
-echo -e "searching for ${group} in /tmp/${group}.log"
-grep ${group} -C 30 --color --group-separator=${separator} /tmp/${group}.log
